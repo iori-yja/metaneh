93468c8fcf460d84f49e75b5b023835be7d8053a
Iori YONEJI <fivo.11235813@gmail.com>
2016-12-09 04:14:46 +0900

    twitter authentication client: callback received

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use] extern crate nickel;
 use std::collections::HashMap;
-use nickel::{Nickel, HttpRouter};
+use nickel::{Nickel, HttpRouter, QueryString, Query};
+use nickel::extensions::Redirect;
 extern crate r2d2;
 extern crate r2d2_sqlite;
 extern crate rustc_serialize;
@@ -17,6 +18,16 @@ pub struct Giant_Root_Node {
     comments: Vec<model::Comment>
 }
 
+fn get_sign_in_param (q: &Query) -> (String, String) {
+    let token = q.get("oauth_token").unwrap();
+    let verifier = q.get("oauth_verifier").unwrap();
+    return (token.to_string(), verifier.to_string());
+}
+
+fn concat_tuple ((s, t): (String, String)) -> String {
+    return s + &t;
+}
+
 fn main() {
     let mut server = Nickel::new();
     let pool = model::establish_resourcepool("test.db");
@@ -26,6 +37,14 @@ fn main() {
         let papers = model::get_all_papers(&pool);
         let comments = model::get_all_comments(&pool);
         return response.render("view/index.tmpl", &Giant_Root_Node { users: users, papers: papers, comments: comments });
+    });
+
+    server.get("/sign-in", middleware! {|_, response|
+        let twitter_sign_in = twitter::start_sign_in();
+        return response.redirect(twitter_sign_in)});
+
+    server.get("/sign-in-callback/", middleware! {|request,response|
+        format!("test{}", concat_tuple(get_sign_in_param(request.query())))
     });
 
     server.listen("127.0.0.1:6767");
--- src/twitter.rs
+++ src/twitter.rs
@@ -11,7 +11,6 @@ use std::path::Path;
 use std::path::PathBuf;
 use rustc_serialize::Decodable;
 use rustc_serialize::json::{self, Json};
-//use oauth_client::Token;
 
 #[derive(Debug, RustcEncodable, RustcDecodable)]
 pub struct AppConfig {
@@ -19,11 +18,20 @@ pub struct AppConfig {
     pub consumer_secret: String,
 }
 
-pub fn read_appconfig () {
+fn read_appconfig () -> AppConfig {
     let mut f = File::open(".config").unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
     let decoded: AppConfig = toml::decode_str(&s).unwrap();
+    return decoded;
 }
 
+fn generate_authorize_url (ac: AppConfig) -> String {
+    let consumer = oauth_client::Token::new(ac.consumer_key, ac.consumer_secret);
+    let request = twitter_api::get_request_token(&consumer).unwrap();
+    return twitter_api::get_authorize_url(&request);
+}
 
+pub fn start_sign_in () -> String {
+    return generate_authorize_url(read_appconfig());
+}
